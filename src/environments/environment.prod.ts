94ac420360b75587f66f8dfc0728be244c227ce8
nictapial <nic.tapial@duocuc.cl>
2024-10-29 12:50:33 -0300

    version 6

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+
+  firebaseConfig: {
+    apiKey: "AIzaSyDpasBq2h7kRIF70qCDTtZOYFX9GFbCgIk",
+    authDomain: "app-mobile-v6.firebaseapp.com",
+    projectId: "app-mobile-v6",
+    storageBucket: "app-mobile-v6.appspot.com",
+    messagingSenderId: "46864661998",
+    appId: "1:46864661998:web:71ce5b43a1f5abcd4f62ff"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  firebaseConfig: {
+    apiKey: "AIzaSyDpasBq2h7kRIF70qCDTtZOYFX9GFbCgIk",
+    authDomain: "app-mobile-v6.firebaseapp.com",
+    projectId: "app-mobile-v6",
+    storageBucket: "app-mobile-v6.appspot.com",
+    messagingSenderId: "46864661998",
+    appId: "1:46864661998:web:71ce5b43a1f5abcd4f62ff"
+  }
 };
 
 /*
